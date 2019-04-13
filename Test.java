91d1d99735dbd17c06bc7ad9fe0f8386efc307f2
DESKTOP-L8DIRFG\yuzuo <kenta.sekine@fox-hound.jp>
2019-04-14 00:43:57 +0900

    Test2クラスを追加

--- Test.java
+++ Test.java
@@ -1,6 +1,16 @@
 class Test{
   
-  public static void main(String[] args){
-    System.out.println("hello world");
-  }
+	public static void main(String[] args){
+    		System.out.println("hello world");
+    		Test2 tes2 = new Test2();
+		int ans = tes2.num();
+		System.out.println(ans);
+	}
+}
+
+class Test2{
+
+	public int num(){
+		return 1;  
+ 	}	
 }
