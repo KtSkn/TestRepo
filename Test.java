199b789d533d0e6e0fa5e58f2616403db3779467
DESKTOP-L8DIRFG\yuzuo <kenta.sekine@fox-hound.jp>
2019-04-14 14:57:37 +0900

    インデントを修正

--- Test.java
+++ Test.java
@@ -1,8 +1,8 @@
 class Test{
   
 	public static void main(String[] args){
-    		System.out.println("hello world");
-    		Test2 tes2 = new Test2();
+    System.out.println("hello world");
+    Test2 tes2 = new Test2();
 		int ans = tes2.num();
 		System.out.println(ans);
 	}
@@ -11,6 +11,6 @@ class Test{
 class Test2{
 
 	public int num(){
-		return 1;  
+	  return 1;  
  	}	
 }
