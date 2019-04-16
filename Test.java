40b0abe36e5d2e658753e5e6e880f291f2e1398c
DESKTOP-L8DIRFG\yuzuo <kenta.sekine@fox-hound.jp>
2019-04-16 23:31:26 +0900

    アニメーションで遊んだ

--- Test.java
+++ Test.java
@@ -1,16 +1,36 @@
 class Test{
-  
+
 	public static void main(String[] args){
     System.out.println("hello world");
     Test2 tes2 = new Test2();
-		int ans = tes2.num();
-		System.out.println(ans);
+    Test3 tes3 = new Test3();
+    Test4 tes4 = new Test4();
+    int ans2 = tes2.num();
+    int ans3 = tes3.num();
+		int ans4 = tes4.num();
+    System.out.println(ans2);
+    System.out.println(ans3);
+    System.out.println(ans4);
 	}
 }
 
 class Test2{
 
 	public int num(){
-	  return 1;  
- 	}	
+	  return 1;
+ 	}
+}
+
+class Test3 {
+
+  public int num(){
+    return 2;
+  }
+}
+
+class Test4 {
+
+  public int num(){
+    return 3;
+  }
 }
